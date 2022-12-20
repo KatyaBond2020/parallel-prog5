316560bc7db451d6e7e2b4f49ce3976167c49981
Ekaterina Bondarenko <72721753+KatyaBond2020@users.noreply.github.com>
2022-12-20 09:49:37 +0800

    Add files via upload

---
+++ 5/Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var count = 0;
+
+			do
+			{
+				int timeToSleep = new Random().Next(1000, 3000);
+				Thread.Sleep(timeToSleep);
+
+				var factory = new ConnectionFactory() { HostName = "localhost" };
+				using(var connection = factory.CreateConnection())
+				using(var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(
+						queue: "dev-queue",
+						durable: true,
+						exclusive: false,
+						autoDelete: false,
+						arguments: null);
+
+					string message = $"mess from publisher, {count}";
+					var body = Encoding.UTF8.GetBytes(message);
+
+					channel.BasicPublish(exchange: "",
+						routingKey: "dev-queue",
+						basicProperties:null,
+						body: body);
+
+					Console.WriteLine($"mess sent, [{count++}]");
+				}
+			} while (true);			
+		}
+	}
+}
